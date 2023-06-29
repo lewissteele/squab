2249423689fae900b0ed51da68dc47a33a4c3103
Lewis Steele <lewissteele254@gmail.com>
2023-06-29 18:59:22 +0100

    Tidy up

--- cmd/add.go
+++ cmd/add.go
@@ -1,6 +1,3 @@
-/*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
@@ -19,7 +16,6 @@ var validate = func(input string) error {
 	return errors.New("Invalid domain name")
 }
 
-// addCmd represents the add command
 var addCmd = &cobra.Command{
 	Use:   "add",
 	Short: "A brief description of your command",
@@ -40,14 +36,4 @@ to quickly create a Cobra application.`,
 
 func init() {
 	rootCmd.AddCommand(addCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// addCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// addCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
